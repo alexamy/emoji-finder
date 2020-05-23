fa033f00439afd97bfe24591f31af175a7d4b249
Alexander Chaschin <achasch@gmail.com>
2020-05-24 02:03:24 +0300

    add naive emojis filter

--- src/App.svelte
+++ src/App.svelte
@@ -3,11 +3,19 @@
   import Emoji from './Emoji.svelte';
   import getEmojis from './helpers/getEmojis';
 
-  const emojis = getEmojis();
+  const emojisFull = getEmojis();
+  let emojis = [...emojisFull];
+
+  const handleInput = e => {
+    const { value } = e.target;
+    emojis = value
+      ? emojisFull.filter(e => e.slug.includes(value))
+      : [...emojisFull];
+  }
 </script>
 
 <main>
-  <Input/>
+  <Input on:input={handleInput} />
   {#each emojis as emoji (emoji.codePoint)}
     <Emoji character={emoji.character} slug={emoji.slug}/>
   {/each}
