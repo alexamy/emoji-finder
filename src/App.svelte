eb5c981c2d3cd96bddc68b4b9a86239cf14d8a56
Alexander Chaschin <achasch@gmail.com>
2020-05-24 21:08:13 +0300

    add scroll event handler

--- src/App.svelte
+++ src/App.svelte
@@ -4,6 +4,7 @@
   import getEmojis from './helpers/getEmojis';
 
   export let maxLength = 20;
+  let scrollY, innerHeight, body;
 
   const emojisFull = [...getEmojis()];
   let emojis = emojisFull;
@@ -21,7 +22,16 @@
   const addEmojis = e => {
     emojisView = emojis.slice(0, emojisView.length + maxLength);
   }
+
+  const handleScroll = e => {
+    const scrollEnd = scrollY + innerHeight;
+    const { scrollHeight } = document.body;
+    const windowEnd = Math.abs(scrollHeight - scrollEnd) < 50;
+    if(windowEnd) addEmojis();
+  }
 </script>
+
+<svelte:window bind:innerHeight bind:scrollY on:scroll={handleScroll}/>
 
 <main>
   <Input on:input={handleInput} />
