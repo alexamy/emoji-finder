46601e3a447fb33f6645e6ba16c3f2f9d8f78e43
Alexander Chaschin <achasch@gmail.com>
2020-05-24 21:14:16 +0300

    extract scroll to bottom logic to ScrollBottom component

--- src/App.svelte
+++ src/App.svelte
@@ -1,10 +1,10 @@
 <script>
+  import ScrollBottom from './ScrollBottom.svelte';
   import Input from './Input.svelte';
   import Emoji from './Emoji.svelte';
   import getEmojis from './helpers/getEmojis';
 
   export let maxLength = 20;
-  let scrollY, innerHeight, body;
 
   const emojisFull = [...getEmojis()];
   let emojis = emojisFull;
@@ -22,28 +22,21 @@
   const addEmojis = e => {
     emojisView = emojis.slice(0, emojisView.length + maxLength);
   }
-
-  const handleScroll = e => {
-    const scrollEnd = scrollY + innerHeight;
-    const { scrollHeight } = document.body;
-    const windowEnd = Math.abs(scrollHeight - scrollEnd) < 50;
-    if(windowEnd) addEmojis();
-  }
 </script>
 
-<svelte:window bind:innerHeight bind:scrollY on:scroll={handleScroll}/>
+<ScrollBottom precision={50} on:scrollToBottom={addEmojis}>
+  <main>
+    <Input on:input={handleInput} />
+    {#if emojisView.length}
+      {#each emojisView as emoji (emoji.codePoint)}
+        <Emoji character={emoji.character} slug={emoji.slug}/>
+      {/each}
+    {:else}
+      No emojis found.
+    {/if}
 
-<main>
-  <Input on:input={handleInput} />
-  {#if emojisView.length}
-    {#each emojisView as emoji (emoji.codePoint)}
-      <Emoji character={emoji.character} slug={emoji.slug}/>
-    {/each}
-  {:else}
-    No emojis found.
-  {/if}
-
-  {#if emojisView.length < emojis.length }
-    <button on:click={addEmojis}>Show more</button>
-  {/if}
-</main>
+    {#if emojisView.length < emojis.length }
+      Loading...
+    {/if}
+  </main>
+</ScrollBottom>
---
+++ src/ScrollBottom.svelte
@@ -0,0 +1,22 @@
+<script>
+  import { createEventDispatcher } from 'svelte';
+
+  const dispatch = createEventDispatcher();
+
+  export let precision;
+  let scrollY, innerHeight;
+
+  const handleScroll = e => {
+    const scrollEnd = scrollY + innerHeight;
+    const { scrollHeight } = document.body;
+
+    const windowEnd = Math.abs(scrollHeight - scrollEnd) < precision;
+    if(windowEnd) {
+      dispatch('scrollToBottom');
+    }
+  }
+</script>
+
+<svelte:window bind:scrollY bind:innerHeight on:scroll={handleScroll}/>
+<slot>
+</slot>
